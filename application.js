7e331215efc6af43367b26f4259e24d394e39d56
Nick Giancola <nick@curiousminds.com>
2011-07-19 00:52:19 -0700

    added setTimeout and instructions for handling Chrome autofill madness

--- application.js
+++ application.js
@@ -11,4 +11,11 @@ $(function() {
 	
 	$('#myinput2').val('changed with js!').change();
 	
+	setTimeout(function() {
+		// Chrome does not fire a change event when autofilling forms
+		// To prevent weird overlap with defaultValue and Chrome autofill,
+		// fire your own change event after 100 or so milliseconds
+		$('#myinput').change();
+	}, 100);
+	
 });
