efccb59d2de89d9234580f9211a609f17eaf93eb
Nick Giancola <nick@curiousminds.com>
2011-07-20 16:16:36 -0700

    added option to prefer html5 placeholder if available, defaulted to off

--- application.js
+++ application.js
@@ -11,11 +11,14 @@ $(function() {
 	
 	$('#myinput2').val('changed with js!').change();
 	
+	
+	// TODO: This should probably go in the plugin itself
 	setTimeout(function() {
 		// Chrome does not fire a change event when autofilling forms
 		// To prevent weird overlap with defaultValue and Chrome autofill,
 		// fire your own change event after 100 or so milliseconds
 		$('#myinput').change();
 	}, 100);
+	
 	
 });
--- jquery.defaultvalue.js
+++ jquery.defaultvalue.js
@@ -15,7 +15,8 @@
       var defaults = {
         'labelText': $label.text(),
         'hideLabel': true,
-        'clearOnFocus': false
+        'clearOnFocus': false,
+				'preferPlaceholder': false
       };
       
       // Allow user to override default options
@@ -24,83 +25,95 @@
       // Hide the label
       if ($label.length > 0 && options['hideLabel'] == true) $label.hide();
       
+			// Check for HTML5 placeholder support in user's browser
+			if (hasPlaceholderSupport() && options['preferPlaceholder']) {
+				// Use HTML5 placeholder
+				$input.attr('placeholder', options['labelText'])
+			} else {
       
-      // Wrap the input with a container div
-      var $wrapper = $('<div></div>');
-      $wrapper.css({
-        'display': 'inline-block',
-        'position': 'relative'
-      });
-      $input.wrap($wrapper);
+	      // Wrap the input with a container div
+	      var $wrapper = $('<div></div>');
+	      $wrapper.css({
+	        'display': 'inline-block',
+	        'position': 'relative'
+	      });
+	      $input.wrap($wrapper);
       
       
-      var $newInput = $input.clone();
-      $newInput.css({
-        'position': 'absolute',
-        'top': 0,
-        'left': 0,
-        'z-index': '100',
-        'background-color': 'transparent',
-        'border-color': 'transparent',
-        'box-shadow': 'none',
-				'background-image': 'none'
-      });
-      $newInput.addClass('defaultvalue_label');
-      $newInput.removeAttr('name').removeAttr('id');
-      $newInput.val(options['labelText']);
-      $newInput.attr('tabindex','-1');
+	      var $newInput = $input.clone();
+	      $newInput.css({
+	        'position': 'absolute',
+	        'top': 0,
+	        'left': 0,
+	        'z-index': '100',
+	        'background-color': 'transparent',
+	        'border-color': 'transparent',
+	        'box-shadow': 'none',
+					'background-image': 'none'
+	      });
+	      $newInput.addClass('defaultvalue_label');
+	      $newInput.removeAttr('name').removeAttr('id');
+	      $newInput.val(options['labelText']);
+	      $newInput.attr('tabindex','-1');
 			
-			// If $newInput is a password field, change it to text so label is readable
-			if ($newInput.attr('type') == 'password') {
-				$newInput.attr('type', 'text');
+				// If $newInput is a password field, change it to text so label is readable
+				if ($newInput.attr('type') == 'password') {
+					$newInput.attr('type', 'text');
+				}
+      
+      
+	      // Do not show label input if the input already has a value in it
+	      if ($input.val() != '') $newInput.hide();
+      
+      
+	      // When the user focuses on our label input
+	      $newInput.focus(function() {
+        
+	        $newInput.addClass('defaultvalue_label_focus');
+	        $input.focus();
+        
+	      });
+      
+      
+	      // Listen for keypress and paste events
+	      $input.bind('keypress paste', function() {
+        
+	        $newInput.hide();
+        
+	      }).bind('keyup', function() {
+        
+	        if ($input.val() == '' && options['clearOnFocus'] == false) $newInput.show();
+        
+	      }).focus(function() {
+        
+	        // When our real input receives focus
+	        $newInput.addClass('defaultvalue_label_focus');
+	        if (options['clearOnFocus'] == true) $newInput.hide();
+        
+	      }).blur(function() {
+        
+	        if ($input.val() == '') $newInput.show();
+	        $newInput.removeClass('defaultvalue_label_focus');
+        
+	      });
+			
+				// Dev can change the value of this input programatically, but must call .change() on it after
+				// We pickup that change here and hide the label
+	      $input.change(function() {
+					if ($input.val() != '') $newInput.hide();
+				});
+      
+	      // Add new input (label input)
+	      $input.before($newInput);
+			
 			}
       
-      
-      // Do not show label input if the input already has a value in it
-      if ($input.val() != '') $newInput.hide();
-      
-      
-      // When the user focuses on our label input
-      $newInput.focus(function() {
-        
-        $newInput.addClass('defaultvalue_label_focus');
-        $input.focus();
-        
-      });
-      
-      
-      // Listen for keypress and paste events
-      $input.bind('keypress paste', function() {
-        
-        $newInput.hide();
-        
-      }).bind('keyup', function() {
-        
-        if ($input.val() == '' && options['clearOnFocus'] == false) $newInput.show();
-        
-      }).focus(function() {
-        
-        // When our real input receives focus
-        $newInput.addClass('defaultvalue_label_focus');
-        if (options['clearOnFocus'] == true) $newInput.hide();
-        
-      }).blur(function() {
-        
-        if ($input.val() == '') $newInput.show();
-        $newInput.removeClass('defaultvalue_label_focus');
-        
-      });
-			
-			// Dev can change the value of this input programatically, but must call .change() on it after
-			// We pickup that change here and hide the label
-      $input.change(function() {
-				if ($input.val() != '') $newInput.hide();
-			});
-      
-      // Add new input (label input)
-      $input.before($newInput);
-      
     });
+
+		function hasPlaceholderSupport() {
+		  var input = document.createElement('input');
+		  return ('placeholder' in input);
+		}
 
   };
 	
